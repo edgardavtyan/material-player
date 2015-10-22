905c24b5d0c8fd09cc15ba69af2c7a01948910fb
Edgar Davtyan <edgar.davtyan@outlook.com>
2015-10-23 02:33:16 +0300

    Load art to parallax header asynchronously

--- app/src/main/java/com/edavtyan/materialplayer/app/activities/AlbumActivity.java
+++ app/src/main/java/com/edavtyan/materialplayer/app/activities/AlbumActivity.java
@@ -1,6 +1,7 @@
 package com.edavtyan.materialplayer.app.activities;
 
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.CursorLoader;
@@ -11,10 +12,14 @@ import com.edavtyan.materialplayer.app.R;
 import com.edavtyan.materialplayer.app.activities.base.CollapsingHeaderListActivity;
 import com.edavtyan.materialplayer.app.adapters.RecyclerViewCursorAdapter;
 import com.edavtyan.materialplayer.app.music.Album;
+import com.edavtyan.materialplayer.app.music.ArtProvider;
+import com.edavtyan.materialplayer.app.music.Metadata;
 import com.edavtyan.materialplayer.app.music.adapters.AlbumTracksAdapter;
 import com.edavtyan.materialplayer.app.music.adapters.TracksAdapter;
 import com.edavtyan.materialplayer.app.music.columns.TrackColumns;
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 public class AlbumActivity extends CollapsingHeaderListActivity {
 
@@ -22,6 +27,25 @@ public class AlbumActivity extends CollapsingHeaderListActivity {
 
     private TracksAdapter tracksAdapter;
     private Album album;
+
+
+    private class ImageLoadTask extends AsyncTask<Integer, Void, File> {
+        @Override
+        protected File doInBackground(Integer... albumIds) {
+            Metadata metadata = Metadata.firstTrackOfAlbum(albumIds[0], AlbumActivity.this);
+            return new ArtProvider().getArt(metadata);
+        }
+
+        @Override
+        protected void onPostExecute(File artFile) {
+            ImageView artView = (ImageView) findViewById(R.id.art);
+            Picasso.with(AlbumActivity.this)
+                    .load(artFile)
+                    .placeholder(R.drawable.fallback_cover)
+                    .error(R.drawable.fallback_cover)
+                    .into(artView);
+        }
+    }
 
 
     @Override
@@ -62,10 +86,6 @@ public class AlbumActivity extends CollapsingHeaderListActivity {
 
     @Override
     public void setImageSource(ImageView imageView) {
-        Picasso.with(this)
-                .load(album.getArt())
-                .placeholder(R.drawable.fallback_cover)
-                .error(R.drawable.fallback_cover)
-                .into(imageView);
+        new ImageLoadTask().execute(album.getId());
     }
 }
