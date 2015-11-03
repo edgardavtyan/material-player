692cd5639f35d3dfb2f093fec3978c563d82af43
Edgar Davtyan <edgar.davtyan@outlook.com>
2015-11-04 00:12:29 +0200

    Cleanup 'DataStorage' class

--- app/src/main/java/com/edavtyan/materialplayer/app/utils/DataStorage.java
+++ app/src/main/java/com/edavtyan/materialplayer/app/utils/DataStorage.java
@@ -3,7 +3,6 @@ package com.edavtyan.materialplayer.app.utils;
 import android.os.Environment;
 
 import java.io.File;
-import java.io.FileOutputStream;
 
 public final class DataStorage {
     private DataStorage() {}
@@ -16,21 +15,5 @@ public final class DataStorage {
 
     static {
         ART_FOLDER.mkdirs();
-    }
-
-
-    public static File readArt(int id) {
-        return new File(ART_FOLDER, Integer.toString(id));
-    }
-
-    public static void saveArt(int id, byte[] buffer) {
-        try {
-            File artFile = readArt(id);
-            FileOutputStream outputStream = new FileOutputStream(artFile);
-            outputStream.write(buffer);
-            outputStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
