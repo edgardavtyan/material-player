e24b49dfd0be7c4751b7a3340195b43fb71825f8
Edgar Davtyan <edgar.davtyan@outlook.com>
2015-11-18 08:55:00 +0200

    Add toolbar to preferences activity

--- app/src/main/java/com/edavtyan/materialplayer/app/activities/PrefActivity.java
+++ app/src/main/java/com/edavtyan/materialplayer/app/activities/PrefActivity.java
@@ -2,6 +2,7 @@ package com.edavtyan.materialplayer.app.activities;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.edavtyan.materialplayer.app.R;
 
@@ -9,6 +10,12 @@ public class PrefActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_pref_toolbar);
         addPreferencesFromResource(R.xml.prefs_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.prefs_title);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        toolbar.setNavigationOnClickListener(view -> onBackPressed());
     }
 }
