4dedaf36c99161f91c8d969829ce05dcbbf2f42c
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-06-02 17:46:19 +0300

    Draw divider on bottom of list item

--- app/src/main/java/com/edavtyan/materialplayer/app/decorators/DividerItemDecoration.java
+++ app/src/main/java/com/edavtyan/materialplayer/app/decorators/DividerItemDecoration.java
@@ -23,6 +23,7 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
         for (int i = 0; i < parent.getChildCount(); i++) {
             View child = parent.getChildAt(i);
             c.drawLine(0, child.getTop(), child.getRight(), child.getTop(), dividerPaint);
+            c.drawLine(0, child.getBottom(), child.getRight(), child.getBottom(), dividerPaint);
             c.save();
         }
     }
