589acce2f544660a610ed5706fdca366a9f01b57
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-06-11 16:39:23 +0300

    Refactor artists list view holder

--- app/src/main/java/com/edavtyan/materialplayer/app/App.java
+++ app/src/main/java/com/edavtyan/materialplayer/app/App.java
@@ -1,23 +1,34 @@
 package com.edavtyan.materialplayer.app;
 
 import android.app.Application;
+import android.content.Context;
 import android.util.Log;
 
 import com.edavtyan.materialplayer.app.logging.FileLogger;
 import com.edavtyan.materialplayer.app.logging.Logger;
+
+import lombok.Getter;
 
 public class App
 		extends Application
 		implements Thread.UncaughtExceptionHandler {
 	private Logger logger;
 
+	//---
+
+	private @Getter static Context context;
+
+	//---
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		logger = new FileLogger();
+		context = getApplicationContext();
 		Thread.setDefaultUncaughtExceptionHandler(this);
 	}
+
+	//---
 
 	@Override
 	public void uncaughtException(Thread thread, Throwable throwable) {
