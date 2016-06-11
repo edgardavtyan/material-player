5a0b9c75672fb336ed09132eb2cd98135a57bd66
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-06-11 23:30:12 +0300

    Refactor playlist track view holder

--- app/src/main/java/com/edavtyan/materialplayer/app/views/Playlist/PlaylistAdapter.java
+++ app/src/main/java/com/edavtyan/materialplayer/app/views/Playlist/PlaylistAdapter.java
@@ -4,23 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.PopupMenu;
-import android.widget.TextView;
 
+import com.edavtyan.materialplayer.app.MusicPlayerService;
 import com.edavtyan.materialplayer.app.R;
 import com.edavtyan.materialplayer.app.views.lib.adapters.RecyclerViewServiceAdapter;
-import com.edavtyan.materialplayer.app.MusicPlayerService;
 
-public class PlaylistAdapter
-		extends RecyclerViewServiceAdapter<PlaylistAdapter.TrackViewHolder> {
-
+public class PlaylistAdapter extends RecyclerViewServiceAdapter<PlaylistTrackViewHolder> {
 	private final Context context;
 
 	public PlaylistAdapter(Context context) {
@@ -40,75 +32,40 @@ public class PlaylistAdapter
 		}
 	};
 
-	/*
-	 * ViewHolder
-	 */
+	@Override
+	public PlaylistTrackViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		View view = LayoutInflater.from(context).inflate(R.layout.listitem_track, parent, false);
+		PlaylistTrackViewHolder holder = new PlaylistTrackViewHolder(context, view);
 
-	public class TrackViewHolder
-			extends RecyclerView.ViewHolder
-			implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
-		private final TextView titleView;
-		private final TextView infoView;
-		private final ImageButton menuButton;
-		private final ImageView nowPlayingIcon;
-
-		public TrackViewHolder(View itemView) {
-			super(itemView);
-			itemView.setOnClickListener(this);
-
-			titleView = (TextView) itemView.findViewById(R.id.title);
-			infoView = (TextView) itemView.findViewById(R.id.info);
-			menuButton = (ImageButton) itemView.findViewById(R.id.menu);
-			nowPlayingIcon = (ImageView) itemView.findViewById(R.id.nowPlaying);
-
-			PopupMenu popupMenu = new PopupMenu(context, menuButton);
-			popupMenu.inflate(R.menu.menu_queue);
-			popupMenu.setOnMenuItemClickListener(this);
-			menuButton.setOnClickListener(view -> popupMenu.show());
-		}
-
-		@Override
-		public void onClick(View view) {
-			if (!isBound) {
-				return;
-			}
-
+		holder.setOnClickListener(itemView -> {
+			if (!isBound) return;
 			int oldPosition = service.getPlayer().getCurrentTrackIndex();
-			service.getPlayer().setCurrentTrackIndex(getAdapterPosition());
+			service.getPlayer().setCurrentTrackIndex(holder.getAdapterPosition());
 			service.getPlayer().prepare();
 			notifyItemChanged(oldPosition);
-		}
+		});
 
-		@Override
-		public boolean onMenuItemClick(MenuItem menuItem) {
-			switch (menuItem.getItemId()) {
+		holder.setOnMenuItemClickListener(item -> {
+			switch (item.getItemId()) {
 			case R.id.menu_remove:
-				service.getPlayer().getQueue().remove(getAdapterPosition());
-				notifyItemRemoved(getAdapterPosition());
+				service.getPlayer().getQueue().remove(holder.getAdapterPosition());
+				notifyItemRemoved(holder.getAdapterPosition());
 				return true;
 
 			default:
 				return false;
 			}
-		}
+		});
+
+		return holder;
 	}
 
 	@Override
-	public TrackViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		View view = LayoutInflater.from(context).inflate(R.layout.listitem_track, parent, false);
-		return new TrackViewHolder(view);
-	}
-
-	@Override
-	public void onBindViewHolder(TrackViewHolder holder, int position) {
+	public void onBindViewHolder(PlaylistTrackViewHolder holder, int position) {
 		if (!isBound) return;
-		holder.titleView.setText(service.getPlayer().getQueue().get(position).getTrackTitle());
-		holder.infoView.setText(service.getPlayer().getQueue().get(position).getAlbumTitle());
-		if (service.getPlayer().getCurrentTrackIndex() == holder.getLayoutPosition()) {
-			holder.nowPlayingIcon.setVisibility(View.VISIBLE);
-		} else {
-			holder.nowPlayingIcon.setVisibility(View.GONE);
-		}
+		holder.setTitle(service.getPlayer().getQueue().get(position).getTrackTitle());
+		holder.setInfo(service.getPlayer().getQueue().get(position).getAlbumTitle());
+		holder.setNowPlaying(service.getPlayer().getCurrentTrackIndex() == holder.getLayoutPosition());
 	}
 
 	@Override
