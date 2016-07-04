812fa35599f2d44f186620bab111d50f8b42344f
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-07-04 03:03:44 +0300

    Add explanatory comments to Amplifier class

--- app/src/main/java/com/edavtyan/materialplayer/components/audioeffects/models/Amplifier.java
+++ app/src/main/java/com/edavtyan/materialplayer/components/audioeffects/models/Amplifier.java
@@ -6,6 +6,11 @@ import android.preference.PreferenceManager;
 
 import com.h6ah4i.android.media.audiofx.IPreAmp;
 
+// This class acts as a decorator for OpenSL amplifier. The OpenSL amplifier accepts float values
+// from 0.0f - no volume, to 2.0f - double volume. This class only allows changing volume
+// between 100% and 200%. It accepts integer values from 0 to 100 and converts them to float,
+// where 0 is 1.0f - 100% volume, and 100 is 2.0f - 200% volume
+
 public class Amplifier {
 	private static final String PREF_STRENGTH = "pref_amplifier_strength";
 	private static final int MAX_STRENGTH = 100;
@@ -29,10 +34,14 @@ public class Amplifier {
 		return MAX_STRENGTH;
 	}
 
+	// Converts float volume to integer.
+	// For example 1.4f -> 40, 1.9 -> 90
 	public int getStrength() {
 		return (int) ((amplifier.getLevel() - 1) * MAX_STRENGTH);
 	}
 
+	// Converts integer volume to float.
+	// For example 40 -> 1.4f, 90 -> 1.9f
 	public void setStrength(int strength) {
 		float strengthFloat = (strength / (float) MAX_STRENGTH) + 1;
 		if (strengthFloat > 2.0f) strengthFloat = 2.0f;
