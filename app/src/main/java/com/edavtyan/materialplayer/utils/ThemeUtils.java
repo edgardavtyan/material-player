8eaa11b596db61af26c11194492427d251f156ca
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-07-10 18:33:41 +0300

    Cleanup ThemeUtils class

--- app/src/main/java/com/edavtyan/materialplayer/utils/ThemeUtils.java
+++ app/src/main/java/com/edavtyan/materialplayer/utils/ThemeUtils.java
@@ -4,20 +4,17 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
 public final class ThemeUtils {
 	private ThemeUtils() {}
 
 
 	private static final String TYPE_THEME = "style";
-
-	public static final String PREF_THEME_BASE = "pref_theme_base";
-	public static final String PREF_THEME_PRIMARY = "pref_theme_primary";
-
-	public static final String THEME_BASE = "AppTheme";
-	public static final String THEME_BASE_DEFAULT = "Light";
-	public static final String THEME_PRIMARY_DEFAULT = "Orange";
+	private static final String PREF_THEME_BASE = "pref_theme_base";
+	private static final String PREF_THEME_PRIMARY = "pref_theme_primary";
+	private static final String THEME_BASE = "AppTheme";
+	private static final String THEME_BASE_DEFAULT = "Light";
+	private static final String THEME_PRIMARY_DEFAULT = "Orange";
 
 
 	public static int fromRes(Context context) {
@@ -25,7 +22,6 @@ public final class ThemeUtils {
 		String themeBase = prefs.getString(PREF_THEME_BASE, THEME_BASE_DEFAULT);
 		String themePrimary = prefs.getString(PREF_THEME_PRIMARY, THEME_PRIMARY_DEFAULT);
 		String themeName = String.format("%s.%s.%s", THEME_BASE, themeBase, themePrimary);
-		Log.d("ThemeUtils", themeName);
 		return context.getResources().getIdentifier(themeName, TYPE_THEME, context.getPackageName());
 	}
 
