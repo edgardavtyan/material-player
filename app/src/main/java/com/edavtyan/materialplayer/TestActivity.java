0f3057e5379301d1ed590bbd45a8931c91fe3a4f
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-07-22 19:22:18 +0300

    Add temp activity to test custom prefs

---
+++ app/src/main/java/com/edavtyan/materialplayer/TestActivity.java
@@ -0,0 +1,11 @@
+package com.edavtyan.materialplayer;
+
+
+import com.edavtyan.materialplayer.lib.activities.BaseToolbarActivity;
+
+public class TestActivity extends BaseToolbarActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.test;
+    }
+}
