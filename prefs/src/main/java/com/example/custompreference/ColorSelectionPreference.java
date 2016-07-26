f0a4eedafc63d7d210cf78edb7a4836614de9f1b
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-07-26 20:24:46 +0300

    Use merge tag in color preference

--- prefs/src/main/java/com/example/custompreference/ColorSelectionPreference.java
+++ prefs/src/main/java/com/example/custompreference/ColorSelectionPreference.java
@@ -2,9 +2,11 @@ package com.example.custompreference;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+import com.example.custompreference.utils.AttributeResolver;
 import com.example.custompreference.utils.PixelConverter;
 
 public class ColorSelectionPreference extends DialogPreference<ColorSelectionController>
@@ -43,6 +45,18 @@ public class ColorSelectionPreference extends DialogPreference<ColorSelectionCon
 
 	@Override
 	protected void createEntryView() {
+		AttributeResolver attrs = new AttributeResolver(context);
+
+		int height = attrs.getDimen(android.R.attr.listPreferredItemHeight);
+		setMinimumHeight(height);
+
+		int paddingLeft = attrs.getDimen(android.R.attr.listPreferredItemPaddingLeft);
+		int paddingRight = attrs.getDimen(android.R.attr.listPreferredItemPaddingRight);
+		setPadding(paddingLeft, 0, paddingRight, 0);
+
+		Drawable background = attrs.getDrawableAttribute(android.R.attr.selectableItemBackground);
+		setBackgroundDrawable(background);
+
 		inflate(context, R.layout.entry_color, this);
 
 		TextView titleView = (TextView) findViewById(R.id.title);
