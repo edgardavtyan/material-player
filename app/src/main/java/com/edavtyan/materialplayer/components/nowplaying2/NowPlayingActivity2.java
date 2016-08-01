caa1ab97f9f08048ff867d9baf91eb5461008bcc
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-08-01 19:42:45 +0300

    Create another now playing activity to test mvp

---
+++ app/src/main/java/com/edavtyan/materialplayer/components/nowplaying2/NowPlayingActivity2.java
@@ -0,0 +1,15 @@
+package com.edavtyan.materialplayer.components.nowplaying2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.edavtyan.materialplayer.R;
+
+public class NowPlayingActivity2 extends AppCompatActivity {
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_nowplaying_2);
+	}
+}
