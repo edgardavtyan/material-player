bdbb719bd1d5d5a7202a315fc47a8010d6e868a4
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-08-12 22:45:08 +0300

    Use MVP album list

--- app/src/main/java/com/edavtyan/materialplayer/lib/adapters/TabPagerFragmentAdapter.java
+++ app/src/main/java/com/edavtyan/materialplayer/lib/adapters/TabPagerFragmentAdapter.java
@@ -4,7 +4,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-import com.edavtyan.materialplayer.components.albums.AlbumsListFragment;
+import com.edavtyan.materialplayer.components.album_mvp.AlbumListFragment;
 import com.edavtyan.materialplayer.components.artist_mvp.ArtistListFragment;
 import com.edavtyan.materialplayer.components.tracks.TracksListFragment;
 
@@ -34,7 +34,7 @@ public class TabPagerFragmentAdapter extends FragmentPagerAdapter {
 			case 0:
 				return new ArtistListFragment();
 			case 1:
-				return new AlbumsListFragment();
+				return new AlbumListFragment();
 			case 2:
 				return new TracksListFragment();
 			default:
