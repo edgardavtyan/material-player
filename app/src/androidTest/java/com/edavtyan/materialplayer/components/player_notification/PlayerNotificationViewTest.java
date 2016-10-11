bfcd9f68899690d1a78d4d9926d5d80774611c5d
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-10-11 20:00:24 +0300

    Unit test notification constructor

--- app/src/androidTest/java/com/edavtyan/materialplayer/components/player_notification/PlayerNotificationViewTest.java
+++ app/src/androidTest/java/com/edavtyan/materialplayer/components/player_notification/PlayerNotificationViewTest.java
@@ -1,29 +1,57 @@
 package com.edavtyan.materialplayer.components.player_notification;
 
+import android.app.PendingIntent;
 import android.support.v4.app.NotificationCompat;
 
 import com.edavtyan.materialplayer.R;
+import com.edavtyan.materialplayer.components.main.MainActivity;
+import com.edavtyan.materialplayer.components.player2.PlayerService;
 import com.edavtyan.materialplayer.lib.AdvancedRemoteViews;
 import com.edavtyan.materialplayer.lib.BaseTest;
 import com.edavtyan.materialplayer.lib.testable.TestableNotificationManager;
+import com.edavtyan.materialplayer.utils.PendingIntents;
 
 import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class PlayerNotificationViewTest extends BaseTest {
-	private PlayerNotificationMvp.View view;
 	private AdvancedRemoteViews remoteViews;
 	private TestableNotificationManager manager;
+	private NotificationCompat.Builder builder;
+	private PendingIntents pendingIntents;
+	private PlayerNotificationMvp.View view;
 
 	@Override public void beforeEach() {
 		super.beforeEach();
 		remoteViews = mock(AdvancedRemoteViews.class);
 		manager = mock(TestableNotificationManager.class);
-		NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-		view = new PlayerNotification(context, remoteViews, manager, builder);
+		builder = spy(new NotificationCompat.Builder(context));
+		pendingIntents = mock(PendingIntents.class);
+		view = new PlayerNotification(context, remoteViews, manager, builder, pendingIntents);
+	}
+
+	@Test public void constructor_buildNotificationWithCorrectValues() {
+		PendingIntent contentIntent = pendingIntents.getActivity(MainActivity.class);
+		when(pendingIntents.getActivity(MainActivity.class)).thenReturn(contentIntent);
+		reset(builder);
+
+		view = new PlayerNotification(context, remoteViews, manager, builder, pendingIntents);
+
+		verify(builder).setSmallIcon(R.drawable.ic_status);
+		verify(builder).setContentIntent(contentIntent);
+		verify(builder).setContent(remoteViews);
+	}
+
+	@Test public void constructor_setClickListeners() {
+		verify(remoteViews).setOnClickBroadcast(R.id.rewind, PlayerService.ACTION_REWIND);
+		verify(remoteViews).setOnClickBroadcast(R.id.play_pause, PlayerService.ACTION_PLAY_PAUSE);
+		verify(remoteViews).setOnClickBroadcast(R.id.fast_forward, PlayerService.ACTION_FAST_FORWARD);
 	}
 
 	@Test public void setTitle_callRemoteViews() {
