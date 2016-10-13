8b64698cc54f0214065564aef66a052b5f169f22
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-10-14 00:11:04 +0300

    Improve Track unit tests

--- app/src/androidTest/java/com/edavtyan/materialplayer/db/TrackTest.java
+++ app/src/androidTest/java/com/edavtyan/materialplayer/db/TrackTest.java
@@ -12,35 +12,43 @@ public class TrackTest extends BaseTest {
 
 	@Override public void beforeEach() {
 		super.beforeEach();
-		first = new Track();
-		second = new Track();
+		first = createTrack();
+		second = createTrack();
 	}
 
 	@Test public void equals_equalTracks_true() {
-		assertThat(createFirstTrack().equals(createSecondTrack())).isTrue();
+		assertThat(first.equals(second)).isTrue();
 	}
 
 	@Test public void equals_idNotEqual_false() {
-		first.setId(0);
 		second.setId(1);
 		assertThat(first.equals(second)).isFalse();
 	}
 
 	@Test public void equals_trackNotEqual_false() {
-		first.setTrack(0);
-		second.setTrack(1);
+		second.setTrack(2);
 		assertThat(first.equals(second)).isFalse();
 	}
 
 	@Test public void equals_titleNotEqual_false() {
-		first.setTitle("first");
 		second.setTitle("second");
 		assertThat(first.equals(second)).isFalse();
 	}
 
-	@Test public void equals_titleIsNull_false() {
-		second.setTitle("first");
+	@Test public void equals_firstTitleIsNull_false() {
+		first.setTitle(null);
 		assertThat(first.equals(second)).isFalse();
+	}
+
+	@Test public void equals_secondTitleIsNull_false() {
+		second.setTitle(null);
+		assertThat(first.equals(second)).isFalse();
+	}
+
+	@Test public void equals_bothTitlesAreNull_true() {
+		first.setTitle(null);
+		second.setTitle(null);
+		assertThat(first.equals(second)).isTrue();
 	}
 
 	@Test public void equals_pathNotEqual_false() {
@@ -49,59 +57,85 @@ public class TrackTest extends BaseTest {
 		assertThat(first.equals(second)).isFalse();
 	}
 
-	@Test public void equals_pathIsNull_false() {
-		second.setPath("path_first");
+	@Test public void equals_firstPathIsNull_false() {
+		first.setPath(null);
 		assertThat(first.equals(second)).isFalse();
 	}
 
+	@Test public void equals_secondPathIsNull_false() {
+		second.setPath(null);
+		assertThat(first.equals(second)).isFalse();
+	}
+
+	@Test public void equals_bothPathsAreNull_true() {
+		first.setPath(null);
+		second.setPath(null);
+		assertThat(first.equals(second)).isTrue();
+	}
+
 	@Test public void equals_dateNotEqual_false() {
-		first.setDateModified(0L);
 		first.setDateModified(1L);
 		assertThat(first.equals(second)).isFalse();
 	}
 
 	@Test public void equals_durationNotEqual_false() {
-		first.setDuration(0L);
 		first.setDuration(1L);
 		assertThat(first.equals(second)).isFalse();
 	}
 
 	@Test public void equals_artistIdNotEqual_false() {
-		first.setArtistId(0);
 		first.setArtistId(1);
 		assertThat(first.equals(second)).isFalse();
 	}
 
 	@Test public void equals_artistTitleNotEqual_false() {
-		first.setArtistTitle("first");
 		first.setArtistTitle("second");
 		assertThat(first.equals(second)).isFalse();
 	}
 
-	@Test public void equals_artistTitleIsNull_false() {
-		second.setArtistTitle("first");
+	@Test public void equals_firstArtistTitleIsNull_false() {
+		first.setArtistTitle(null);
 		assertThat(first.equals(second)).isFalse();
 	}
 
+	@Test public void equals_secondArtistTitleIsNull_false() {
+		second.setArtistTitle(null);
+		assertThat(first.equals(second)).isFalse();
+	}
+
+	@Test public void equals_bothArtistTitlesAreNull_true() {
+		first.setArtistTitle(null);
+		second.setArtistTitle(null);
+		assertThat(first.equals(second)).isTrue();
+	}
+
 	@Test public void equals_albumIdNotEqual_false() {
-		first.setAlbumId(0);
 		first.setAlbumId(1);
 		assertThat(first.equals(second)).isFalse();
 	}
 
 	@Test public void equals_albumTitleNotEqual_false() {
-		first.setAlbumTitle("first");
 		first.setAlbumTitle("second");
 		assertThat(first.equals(second)).isFalse();
 	}
 
-	@Test public void equals_albumTitleIsNull_false() {
-		second.setAlbumTitle("first");
+	@Test public void equals_firstAlbumTitleIsNull_false() {
+		first.setAlbumTitle(null);
 		assertThat(first.equals(second)).isFalse();
 	}
 
+	@Test public void equals_secondAlbumTitleIsNull_false() {
+		second.setAlbumTitle(null);
+		assertThat(first.equals(second)).isFalse();
+	}
+
+	@Test public void equals_bothAlbumTitlesAreNull_true() {
+		first.setAlbumTitle(null);
+		second.setAlbumTitle(null);
+		assertThat(first.equals(second)).isTrue();
+	}
+
 	@Test public void equals_queueIndexNotEqual_false() {
-		first.setQueueIndex(0);
 		first.setQueueIndex(1);
 		assertThat(first.equals(second)).isFalse();
 	}
@@ -112,7 +146,6 @@ public class TrackTest extends BaseTest {
 	}
 
 	@Test public void toString_returnFormattedString() {
-		first = createFirstTrack();
 		String trackAsString =
 				"Track("
 				+ "id=0, "
@@ -135,38 +168,30 @@ public class TrackTest extends BaseTest {
 	}
 
 	@Test public void hashCode_nullTitle_equalHashCode() {
-		first = createFirstTrack();
 		first.setTitle(null);
-		second = createSecondTrack();
 		second.setTitle(null);
 		assertThat(first.hashCode()).isEqualTo(second.hashCode());
 	}
 
 	@Test public void hashCode_nullPath_equalHashCode() {
-		first = createFirstTrack();
 		first.setPath(null);
-		second = createSecondTrack();
 		second.setPath(null);
 		assertThat(first.hashCode()).isEqualTo(second.hashCode());
 	}
 
 	@Test public void hashCode_nullArtistTitle_equalHashCode() {
-		first = createFirstTrack();
 		first.setArtistTitle(null);
-		second = createSecondTrack();
 		second.setArtistTitle(null);
 		assertThat(first.hashCode()).isEqualTo(second.hashCode());
 	}
 
 	@Test public void hashCode_nullAlbumTitle_equalHashCode() {
-		first = createFirstTrack();
 		first.setAlbumTitle(null);
-		second = createSecondTrack();
 		second.setAlbumTitle(null);
 		assertThat(first.hashCode()).isEqualTo(second.hashCode());
 	}
 
-	private Track createFirstTrack() {
+	private Track createTrack() {
 		Track first = new Track();
 		first.setId(0);
 		first.setTrack(1);
@@ -180,21 +205,5 @@ public class TrackTest extends BaseTest {
 		first.setAlbumTitle("album");
 		first.setQueueIndex(6);
 		return first;
-	}
-
-	private Track createSecondTrack() {
-		Track second = new Track();
-		second.setId(0);
-		second.setTrack(1);
-		second.setTitle("title");
-		second.setPath("path");
-		second.setDateModified(2L);
-		second.setDuration(3L);
-		second.setArtistId(4);
-		second.setArtistTitle("artist");
-		second.setAlbumId(5);
-		second.setAlbumTitle("album");
-		second.setQueueIndex(6);
-		return second;
 	}
 }
