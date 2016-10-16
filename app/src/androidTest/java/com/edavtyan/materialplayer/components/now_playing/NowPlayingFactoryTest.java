58d35f7c53c2555cdcae7c393baf2f3f0f113c03
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-10-16 23:22:00 +0300

    Fix NowPlayingFactory unit test

--- app/src/androidTest/java/com/edavtyan/materialplayer/components/now_playing/NowPlayingFactoryTest.java
+++ app/src/androidTest/java/com/edavtyan/materialplayer/components/now_playing/NowPlayingFactoryTest.java
@@ -1,9 +1,15 @@
 package com.edavtyan.materialplayer.components.now_playing;
 
+import android.support.design.widget.FloatingActionButton;
+import android.widget.SeekBar;
+
+import com.edavtyan.materialplayer.R;
+import com.edavtyan.materialplayer.lib.testable.TestableImageButton;
 import com.edavtyan.materialplayer.testlib.tests.FactoryTest;
 
 import org.junit.Test;
 
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 public class NowPlayingFactoryTest extends FactoryTest {
@@ -11,9 +17,21 @@ public class NowPlayingFactoryTest extends FactoryTest {
 
 	@Override public void beforeEach() {
 		super.beforeEach();
-		NowPlayingMvp.View view = mock(NowPlayingMvp.View.class);
-		NowPlayingActivity activity = mock(NowPlayingActivity.class);
-		nowPlayingFactory = new NowPlayingFactory(activity, view);
+		runOnUiThread(() -> {
+			NowPlayingMvp.View view = mock(NowPlayingMvp.View.class);
+
+			NowPlayingActivity activity = mock(NowPlayingActivity.class);
+			doReturn(context.getTheme()).when(activity).getTheme();
+			doReturn(new TestableImageButton(context)).when(activity).findView(R.id.shuffle);
+			doReturn(new TestableImageButton(context)).when(activity).findView(R.id.rewind);
+			doReturn(new TestableImageButton(context)).when(activity).findView(R.id.playPause);
+			doReturn(new TestableImageButton(context)).when(activity).findView(R.id.fastForward);
+			doReturn(new TestableImageButton(context)).when(activity).findView(R.id.repeat);
+			doReturn(new FloatingActionButton(context)).when(activity).findView(R.id.fab);
+			doReturn(new SeekBar(context)).when(activity).findView(R.id.seekbar);
+
+			nowPlayingFactory = new NowPlayingFactory(activity, view);
+		});
 	}
 
 	@Test public void testProviders() throws Exception {
