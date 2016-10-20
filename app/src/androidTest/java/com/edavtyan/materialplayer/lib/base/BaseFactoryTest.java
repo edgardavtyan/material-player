8577bde12b1a971f1cd4d423f6a1a3d96b19cccc
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-10-20 05:23:40 +0300

    Unit test BaseFactory

---
+++ app/src/androidTest/java/com/edavtyan/materialplayer/lib/base/BaseFactoryTest.java
@@ -0,0 +1,26 @@
+package com.edavtyan.materialplayer.lib.base;
+
+import com.edavtyan.materialplayer.testlib.tests.FactoryTest;
+
+import org.junit.Test;
+
+public class BaseFactoryTest extends FactoryTest {
+	private BaseFactory baseFactory;
+
+	@Override public void beforeEach() {
+		super.beforeEach();
+		baseFactory = new BaseFactory(context);
+	}
+
+	@Test public void testProviders() throws Exception {
+		testFactoryMethod(baseFactory::provideContext);
+		testFactoryMethod(baseFactory::provideNavigator);
+		testFactoryMethod(baseFactory::provideArtProvider);
+		testFactoryMethod(baseFactory::provideMusicTagReader);
+		testFactoryMethod(baseFactory::provideDataStorage);
+		testFactoryMethod(baseFactory::provideBitmapFactory);
+		testFactoryMethod(baseFactory::provideThemeUtils);
+		testFactoryMethod(baseFactory::providePrefs);
+		testFactoryMethod(baseFactory::provideBasePrefs);
+	}
+}
