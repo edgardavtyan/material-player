8ef00b2d37dbcb4e8d688a1ef85c96a566711b6a
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-10-21 00:21:54 +0300

    Fix DBTest on API 21+

--- app/src/androidTest/java/com/edavtyan/materialplayer/testlib/tests/DBTest.java
+++ app/src/androidTest/java/com/edavtyan/materialplayer/testlib/tests/DBTest.java
@@ -1,6 +1,7 @@
 package com.edavtyan.materialplayer.testlib.tests;
 
 import android.content.ContentProvider;
+import android.content.pm.ProviderInfo;
 import android.provider.MediaStore;
 import android.test.mock.MockContentResolver;
 
@@ -9,7 +10,9 @@ import static org.mockito.Mockito.when;
 
 public class DBTest extends BaseTest {
 	protected <TProvider extends ContentProvider> void initProvider(TProvider provider) {
-		provider.attachInfo(context, null);
+		ProviderInfo providerInfo = new ProviderInfo();
+		providerInfo.authority = MediaStore.AUTHORITY;
+		provider.attachInfo(context, providerInfo);
 		MockContentResolver mockContentResolver = spy(new MockContentResolver());
 		mockContentResolver.addProvider(MediaStore.AUTHORITY, provider);
 		when(context.getContentResolver()).thenReturn(mockContentResolver);
