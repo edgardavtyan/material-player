e459dcfc0d8ee4573a16552933baa1417b9e757c
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-12-25 05:53:21 +0200

    Fix out of memory error when loading album arts

--- app/src/main/java/com/edavtyan/materialplayer/lib/testable/TestableBitmapFactory.java
+++ app/src/main/java/com/edavtyan/materialplayer/lib/testable/TestableBitmapFactory.java
@@ -5,6 +5,10 @@ import android.graphics.BitmapFactory;
 
 public class TestableBitmapFactory {
 	public Bitmap fromPath(String path) {
-		return BitmapFactory.decodeFile(path);
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inPreferredConfig = Bitmap.Config.RGB_565;
+		options.inSampleSize = 2;
+		options.inDither = true;
+		return BitmapFactory.decodeFile(path, options);
 	}
 }
