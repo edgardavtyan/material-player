f88c5afbabbbad6a49e7d8ff850b753cd65e204e
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-01-02 18:49:52 +0200

    Use remote views directly

--- app/src/main/java/com/edavtyan/materialplayer/components/player_notification/PlayerNotification.java
+++ app/src/main/java/com/edavtyan/materialplayer/components/player_notification/PlayerNotification.java
@@ -3,14 +3,12 @@ package com.edavtyan.materialplayer.components.player_notification;
 import android.app.Notification;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
 import com.edavtyan.materialplayer.R;
 import com.edavtyan.materialplayer.components.main.MainActivity;
 import com.edavtyan.materialplayer.components.player.PlayerService;
 import com.edavtyan.materialplayer.lib.AdvancedRemoteViews;
-import com.edavtyan.materialplayer.lib.testable.TestableBitmapFactory;
 import com.edavtyan.materialplayer.lib.testable.TestableNotificationManager;
 import com.edavtyan.materialplayer.utils.PendingIntents;
 
@@ -20,6 +18,8 @@ public class PlayerNotification implements PlayerNotificationMvp.View {
 	public static final int NOTIFICATION_ID = 1;
 
 	private final Context context;
+	private final AdvancedRemoteViews normalRemoteViews;
+	private final AdvancedRemoteViews bigRemoteViews;
 	private final TestableNotificationManager manager;
 	private final @Getter Notification notification;
 
@@ -32,6 +32,8 @@ public class PlayerNotification implements PlayerNotificationMvp.View {
 			PendingIntents pendingIntents
 	) {
 		this.context = context;
+		this.normalRemoteViews = normalRemoteViews;
+		this.bigRemoteViews = bigRemoteViews;
 		this.manager = manager;
 		this.notification = builder
 				.setSmallIcon(R.drawable.ic_status)
@@ -51,34 +53,34 @@ public class PlayerNotification implements PlayerNotificationMvp.View {
 	}
 
 	@Override public void setTitle(String title) {
-		notification.contentView.setTextViewText(R.id.title, title);
-		notification.bigContentView.setTextViewText(R.id.title, title);
+		normalRemoteViews.setTextViewText(R.id.title, title);
+		bigRemoteViews.setTextViewText(R.id.title, title);
 	}
 
 	@Override public void setInfo(String artist, String album) {
 		String info = context.getString(R.string.nowplaying_info_pattern, artist, album);
-		notification.contentView.setTextViewText(R.id.info, info);
-		notification.bigContentView.setTextViewText(R.id.artist, artist);
-		notification.bigContentView.setTextViewText(R.id.album, album);
+		normalRemoteViews.setTextViewText(R.id.info, info);
+		bigRemoteViews.setTextViewText(R.id.artist, artist);
+		bigRemoteViews.setTextViewText(R.id.album, album);
 	}
 
 	@Override public void setArt(Bitmap art) {
 		if (art != null) {
-			notification.contentView.setImageViewBitmap(R.id.art, art);
-			notification.bigContentView.setImageViewBitmap(R.id.art, art);
+			normalRemoteViews.setImageViewBitmap(R.id.art, art);
+			bigRemoteViews.setImageViewBitmap(R.id.art, art);
 		} else {
-			notification.contentView.setImageViewResource(R.id.art, R.drawable.fallback_cover);
-			notification.bigContentView.setImageViewResource(R.id.art, R.drawable.fallback_cover);
+			normalRemoteViews.setImageViewResource(R.id.art, R.drawable.fallback_cover);
+			bigRemoteViews.setImageViewResource(R.id.art, R.drawable.fallback_cover);
 		}
 	}
 
 	@Override public void setIsPlaying(boolean isPlaying) {
 		if (isPlaying) {
-			notification.contentView.setImageViewResource(R.id.play_pause, R.drawable.ic_pause);
-			notification.bigContentView.setImageViewResource(R.id.play_pause, R.drawable.ic_pause);
+			normalRemoteViews.setImageViewResource(R.id.play_pause, R.drawable.ic_pause);
+			bigRemoteViews.setImageViewResource(R.id.play_pause, R.drawable.ic_pause);
 		} else {
-			notification.contentView.setImageViewResource(R.id.play_pause, R.drawable.ic_play);
-			notification.bigContentView.setImageViewResource(R.id.play_pause, R.drawable.ic_play);
+			normalRemoteViews.setImageViewResource(R.id.play_pause, R.drawable.ic_play);
+			bigRemoteViews.setImageViewResource(R.id.play_pause, R.drawable.ic_play);
 		}
 	}
 
