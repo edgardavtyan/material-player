f080e39d60cbe39c0ccca155cdfd42bea1a08e05
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-01-02 19:36:13 +0200

    Remove unused empty method

--- app/src/main/java/com/edavtyan/materialplayer/lib/mvp/list/ListFragment.java
+++ app/src/main/java/com/edavtyan/materialplayer/lib/mvp/list/ListFragment.java
@@ -21,11 +21,6 @@ public abstract class ListFragment<TPresenter extends ListMvp.Presenter> extends
 		presenter.onCreate();
 	}
 
-	@Override
-	public void onCreate(@Nullable Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
-
 	@Nullable
 	@Override
 	public View onCreateView(
