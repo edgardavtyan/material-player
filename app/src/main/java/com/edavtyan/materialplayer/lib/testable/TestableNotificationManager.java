f0cdac4f8368da22ba63f89c7bfbc4adaf8d0979
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-01-11 00:01:32 +0200

    Simplify testable notification manager

--- app/src/main/java/com/edavtyan/materialplayer/lib/testable/TestableNotificationManager.java
+++ app/src/main/java/com/edavtyan/materialplayer/lib/testable/TestableNotificationManager.java
@@ -1,13 +1,16 @@
 package com.edavtyan.materialplayer.lib.testable;
 
+import android.app.Notification;
 import android.support.v4.app.NotificationManagerCompat;
 
-import lombok.experimental.Delegate;
-
 public class TestableNotificationManager {
-	private final @Delegate NotificationManagerCompat manager;
+	private final NotificationManagerCompat manager;
 
 	public TestableNotificationManager(NotificationManagerCompat manager) {
 		this.manager = manager;
+	}
+
+	public void notify(int id, Notification notification) {
+		manager.notify(id, notification);
 	}
 }
