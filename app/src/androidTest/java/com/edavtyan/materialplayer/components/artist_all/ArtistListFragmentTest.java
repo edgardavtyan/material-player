9d2dbf7ed3998fdb4f490de674a76241048a136b
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-01-19 23:55:57 +0200

    Speed up artist list fragment tests

--- app/src/androidTest/java/com/edavtyan/materialplayer/components/artist_all/ArtistListFragmentTest.java
+++ app/src/androidTest/java/com/edavtyan/materialplayer/components/artist_all/ArtistListFragmentTest.java
@@ -1,5 +1,6 @@
 package com.edavtyan.materialplayer.components.artist_all;
 
+import android.annotation.SuppressLint;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -11,32 +12,38 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@SuppressLint("StaticFieldLeak")
 public class ArtistListFragmentTest extends FragmentTest2 {
-	private ArtistListAdapter adapter;
-	private ArtistListMvp.Presenter presenter;
-	private Navigator navigator;
-	private ArtistListFragment fragment;
+	private static ArtistListAdapter adapter;
+	private static ArtistListMvp.Presenter presenter;
+	private static Navigator navigator;
+	private static ArtistListFragment fragment;
 
 	@Override
 	public void beforeEach() {
 		super.beforeEach();
 
-		adapter = mock(ArtistListAdapter.class);
-		presenter = mock(ArtistListMvp.Presenter.class);
-		navigator = mock(Navigator.class);
+		if (fragment == null) {
+			adapter = mock(ArtistListAdapter.class);
+			presenter = mock(ArtistListMvp.Presenter.class);
+			navigator = mock(Navigator.class);
 
-		ArtistListFactory factory = mock(ArtistListFactory.class);
-		when(factory.getAdapter()).thenReturn(adapter);
-		when(factory.getPresenter()).thenReturn(presenter);
-		when(factory.getNavigator()).thenReturn(navigator);
+			ArtistListFactory factory = mock(ArtistListFactory.class);
+			when(factory.getAdapter()).thenReturn(adapter);
+			when(factory.getPresenter()).thenReturn(presenter);
+			when(factory.getNavigator()).thenReturn(navigator);
 
-		app.setArtistListFactory(factory);
+			app.setArtistListFactory(factory);
 
-		fragment = new ArtistListFragment();
-		initFragment(fragment);
+			fragment = new ArtistListFragment();
+			initFragment(fragment);
+		} else {
+			reset(adapter, presenter, navigator);
+		}
 	}
 
 	@Test
