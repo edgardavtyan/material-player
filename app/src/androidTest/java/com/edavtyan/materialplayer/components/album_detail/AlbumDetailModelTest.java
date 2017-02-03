659946c2ce1f2f0573a9b5dd04fdb98380536946
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-02-03 23:55:40 +0200

    Add unit tests for AlbumDetailModel

--- app/src/androidTest/java/com/edavtyan/materialplayer/components/album_detail/AlbumDetailModelTest.java
+++ app/src/androidTest/java/com/edavtyan/materialplayer/components/album_detail/AlbumDetailModelTest.java
@@ -2,13 +2,18 @@ package com.edavtyan.materialplayer.components.album_detail;
 
 import android.graphics.Bitmap;
 
+import com.edavtyan.materialplayer.db.Album;
 import com.edavtyan.materialplayer.db.AlbumDB;
+import com.edavtyan.materialplayer.db.Track;
 import com.edavtyan.materialplayer.db.TrackDB;
 import com.edavtyan.materialplayer.lib.album_art.AlbumArtProvider;
 import com.edavtyan.materialplayer.lib.mvp.list.CompactListPref;
 import com.edavtyan.materialplayer.testlib.tests.BaseTest;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
@@ -22,14 +27,15 @@ public class AlbumDetailModelTest extends BaseTest {
 	private AlbumDetailModel model;
 	private TrackDB trackDB;
 	private AlbumArtProvider albumArtProvider;
+	private AlbumDB albumDB;
 
 	@Override
 	public void beforeEach() {
 		super.beforeEach();
 
 		AlbumDetailActivity view = mock(AlbumDetailActivity.class);
-		AlbumDB albumDB = mock(AlbumDB.class);
 		CompactListPref prefs = mock(CompactListPref.class);
+		albumDB = mock(AlbumDB.class);
 		trackDB = mock(TrackDB.class);
 		albumArtProvider = mock(AlbumArtProvider.class);
 
@@ -40,6 +46,34 @@ public class AlbumDetailModelTest extends BaseTest {
 	public void update_callTracksDB() {
 		model.update();
 		verify(trackDB).getTracksWithAlbumId(ALBUM_ID);
+	}
+
+	@Test
+	public void getAlbum_returnAlbumFromDB() {
+		Album album = new Album();
+		when(albumDB.getAlbumWithAlbumId(ALBUM_ID)).thenReturn(album);
+		assertThat(model.getAlbum()).isSameAs(album);
+	}
+
+	@Test
+	public void getTotalAlbumDuration_returnAllTracksDuration() {
+		Track track1 = new Track();
+		Track track2 = new Track();
+		Track track3 = new Track();
+		track1.setDuration(300);
+		track2.setDuration(400);
+		track3.setDuration(500);
+
+		List<Track> tracks = new ArrayList<>();
+		tracks.add(track1);
+		tracks.add(track2);
+		tracks.add(track3);
+
+		when(trackDB.getTracksWithAlbumId(ALBUM_ID)).thenReturn(tracks);
+
+		model.update();
+
+		assertThat(model.getTotalAlbumDuration()).isEqualTo(1200);
 	}
 
 	@Test
