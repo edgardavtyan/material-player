ca05bfc085917b8d8190a9c9fe6516c75cfb007f
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-06-16 00:02:43 +0300

    Add base SearchFragment

--- app/src/main/java/com/edavtyan/materialplayer/components/search/artist/SearchArtistFragment.java
+++ app/src/main/java/com/edavtyan/materialplayer/components/search/artist/SearchArtistFragment.java
@@ -1,63 +1,21 @@
 package com.edavtyan.materialplayer.components.search.artist;
 
-import android.content.Context;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.edavtyan.materialplayer.R;
-import com.edavtyan.materialplayer.components.search.SearchActivity;
+import com.edavtyan.materialplayer.components.search.base.SearchFragment;
 import com.edavtyan.materialplayer.db.Artist;
-import com.edavtyan.materialplayer.lib.base.BaseFragment;
 
 import java.util.List;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-public class SearchArtistFragment extends BaseFragment
-		implements SearchActivity.OnSearchQueryChangedListener {
-	@BindView(R.id.list) RecyclerView list;
+public class SearchArtistFragment extends SearchFragment {
 
 	private SearchArtistAdapter adapter;
-	private SearchActivity activity;
 	private SearchArtistPresenter presenter;
 
-	@Nullable
 	@Override
-	public View onCreateView(
-			LayoutInflater inflater,
-			@Nullable ViewGroup container,
-			@Nullable Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_list, container, false);
-
-		ButterKnife.bind(this, view);
-
+	protected void onPostCreateView() {
 		SearchArtistFactory factory = app.getSearchFactory(getContext(), this);
 		presenter = factory.getPresenter();
 		adapter = factory.getAdapter();
-
-		list.setAdapter(adapter);
-		list.setLayoutManager(new LinearLayoutManager(getContext()));
-
-		return view;
-	}
-
-	@Override
-	public void onAttach(Context context) {
-		super.onAttach(context);
-		activity = (SearchActivity) context;
-		activity.addOnSearchQueryChangedListener(this);
-	}
-
-	@Override
-	public void onDetach() {
-		super.onDetach();
-		activity.removeOnSearchQueryChangedListener(this);
+		setAdapter(adapter);
 	}
 
 	public void updateArtists(List<Artist> artists) {
