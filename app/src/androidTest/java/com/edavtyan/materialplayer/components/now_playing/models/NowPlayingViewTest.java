d444894cefb84af51ba8e1991f1ddcfb43a36b00
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-11-20 03:26:29 +0200

    Remove unused field

--- app/src/androidTest/java/com/edavtyan/materialplayer/components/now_playing/models/NowPlayingViewTest.java
+++ app/src/androidTest/java/com/edavtyan/materialplayer/components/now_playing/models/NowPlayingViewTest.java
@@ -6,7 +6,6 @@ import com.edavtyan.materialplayer.components.now_playing.NowPlayingActivity;
 import com.edavtyan.materialplayer.components.now_playing.NowPlayingFactory;
 import com.edavtyan.materialplayer.components.now_playing.NowPlayingMvp;
 import com.edavtyan.materialplayer.testlib.tests.ActivityTest;
-import com.edavtyan.materialplayer.utils.AppColors;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -15,14 +14,12 @@ import static org.mockito.Mockito.when;
 public abstract class NowPlayingViewTest extends ActivityTest {
 	protected static NowPlayingActivity activity;
 
-	protected AppColors colors;
 	protected NowPlayingMvp.Presenter presenter;
 
 	@Override
 	public void beforeEach() {
 		super.beforeEach();
 
-		colors = new AppColors(context);
 		presenter = mock(NowPlayingMvp.Presenter.class);
 
 		NowPlayingFactory factory = mock(NowPlayingFactory.class);
