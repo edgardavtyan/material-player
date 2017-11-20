fe039a43a032bcfb916fc6bb36195668a7994564
Edgar Davtyan <edgar.davtyan@outlook.com>
2017-11-20 02:17:46 +0200

    Remove unused method 'save' from BitmapFileStorage

--- app/src/main/java/com/edavtyan/materialplayer/lib/file_storage/BitmapFileStorage.java
+++ app/src/main/java/com/edavtyan/materialplayer/lib/file_storage/BitmapFileStorage.java
@@ -4,21 +4,12 @@ import android.graphics.Bitmap;
 
 import com.edavtyan.materialplayer.lib.testable.TestableBitmapFactory;
 
-import java.io.ByteArrayOutputStream;
-
 public class BitmapFileStorage extends BaseFileStorage {
 	private final TestableBitmapFactory bitmapFactory;
 
 	public BitmapFileStorage(String dirName) {
 		super(dirName);
 		bitmapFactory = new TestableBitmapFactory();
-	}
-
-	public void save(String filename, Bitmap bitmap) {
-		ByteArrayOutputStream stream = new ByteArrayOutputStream();
-		bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-		byte[] bitmapBytes = stream.toByteArray();
-		saveBytes(filename, bitmapBytes);
 	}
 
 	public Bitmap load(String filename) {
