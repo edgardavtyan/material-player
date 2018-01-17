6568dd44c45182f179054ba36e674cd4ca7968ba
Edgar Davtyan <edgar.davtyan@outlook.com>
2018-01-17 20:02:43 +0200

    Add placeholder ui style splash screen

--- app/src/main/java/com/edavtyan/materialplayer/modular/activity/ModularActivity.java
+++ app/src/main/java/com/edavtyan/materialplayer/modular/activity/ModularActivity.java
@@ -1,8 +1,11 @@
 package com.edavtyan.materialplayer.modular.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 import com.edavtyan.materialplayer.lib.theme.ThemeColors;
 import com.edavtyan.materialplayer.lib.theme.ThemeableScreen;
@@ -12,6 +15,12 @@ import java.util.ArrayList;
 
 public abstract class ModularActivity extends AppCompatActivity implements ThemeableScreen {
 	private final ArrayList<UniversalViewModule> modules = new ArrayList<>();
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
+	}
 
 	@Override
 	public void onStart() {
