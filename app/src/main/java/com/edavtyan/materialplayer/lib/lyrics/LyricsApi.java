de8b69de6ac0d0d84231d64b04973b0325cd417d
Edgar Davtyan <edgar.davtyan@outlook.com>
2018-07-22 14:12:32 +0300

    Encode artist and track strings

--- app/src/main/java/com/edavtyan/materialplayer/lib/lyrics/LyricsApi.java
+++ app/src/main/java/com/edavtyan/materialplayer/lib/lyrics/LyricsApi.java
@@ -3,13 +3,15 @@ package com.edavtyan.materialplayer.lib.lyrics;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class LyricsApi {
 	private static final String URL_TEMPLATE = "http://lyrics.wikia.com/wiki/%s:%s";
 
 	public String getLyrics(String artist, String track) {
 		try {
-			String url = String.format(URL_TEMPLATE, artist, track);
+			String url = String.format(URL_TEMPLATE, encode(artist), encode(track));
 			String lyrics = Jsoup.connect(url)
 								 .get()
 								 .select(".lyricbox")
@@ -19,6 +21,16 @@ public class LyricsApi {
 			return lyrics;
 		} catch (IOException e) {
 			return null;
+		}
+	}
+
+	private String encode(String str) {
+		try {
+			String strSpaces = str.replace(" ", "_");
+			String strEncoded = URLEncoder.encode(strSpaces, "UTF-8");
+			return strEncoded;
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
