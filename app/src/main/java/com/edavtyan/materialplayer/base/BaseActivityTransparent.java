e1d05293804dc28b2757ee15c0377678afaa8a4b
Edgar Davtyan <edgar.davtyan@outlook.com>
2018-11-10 17:20:57 +0200

    Apply window transparency via code

--- app/src/main/java/com/edavtyan/materialplayer/base/BaseActivityTransparent.java
+++ app/src/main/java/com/edavtyan/materialplayer/base/BaseActivityTransparent.java
@@ -1,15 +1,12 @@
 package com.edavtyan.materialplayer.base;
 
-import com.edavtyan.materialplayer.R;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 public abstract class BaseActivityTransparent extends BaseActivity {
 	@Override
-	protected int getThemeNormal() {
-		return R.style.AppTheme_Transparent;
-	}
-
-	@Override
-	protected int getThemeDark() {
-		return R.style.AppTheme_Dark_Transparent;
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().setBackgroundDrawableResource(android.R.color.transparent);
 	}
 }
